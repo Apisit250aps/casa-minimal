8db302aa1d4c74bc7693addf6ff5cc206a0bf568
YUapisit <aps.apisit250@gmail.com>
2025-09-02 07:32:53 +0700

    feat: add database collections for listings, listing images, amenities, favorites, tags, and listing tags

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,22 @@
 import client from '@/lib/db'
+import {
+  Amenity,
+  Favorite,
+  Listing,
+  ListingAmenity,
+  ListingImage,
+  ListingTag,
+  Tag,
+} from '@/schema'
 import { type User } from 'next-auth'
-
 const db = client.db()
 
 export const users = db.collection<User>('users')
+export const listings = db.collection<Listing>('listings')
+export const listingImages = db.collection<ListingImage>('listing_images')
+export const amenities = db.collection<Amenity>('amenities')
+export const listingAmenities =
+  db.collection<ListingAmenity>('listing_amenities')
+export const favorites = db.collection<Favorite>('favorites')
+export const tags = db.collection<Tag>('tags')
+export const listingTags = db.collection<ListingTag>('listing_tags')
